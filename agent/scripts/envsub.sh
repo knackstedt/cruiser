91518742f66b99bfed0a6f85b4748c348dbde2e6
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-07-09 11:40:13 -0400

    Add environment substitution script

---
+++ agent/scripts/envsub.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This script reads all files of a given glob and environment substitutes
+# all occurences of ${SOME_VAR}. If a variable isn't defined, the script
+# will print to stderr the variable that wasn't replaced, and exit with a
+# failure code.
+
+for file in ./*.yaml
+do
+    tmp="You shouldnt see this text"
+
+    # Use regex to find all instances of ${VAR_FOO}
+    tmp=$(perl -pe 's/\$\{([_A-Z]+)\}/defined $ENV{$1} ? $ENV{$1} : "\${$1}"/eg' < $file > "$file.tmp")
+    tmp=$(cat $file.tmp)
+    rm "$file.tmp"
+
+    # Use grep to check for any leftover ${less-strict%%vals}
+    err=$(echo "$tmp" | grep -E '\${[^}]*}')
+
+    # If something wasn't substitited, we throw that as an error
+    if [ ${#err} -ge 1 ]; then
+        >&2 echo "$tmp" | grep --color -E '\${[^}]*}'
+        exit 1;
+    fi
+
+    # All substitutions happened successfully -- patch the file.
+    echo "$tmp" > $file
+done
