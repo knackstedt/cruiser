16b758fe0d04da014f1540808cdc6bcb22b963fb
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-07-09 21:52:55 -0400

    Add TS types for schema

---
+++ client/types/environment.ts
@@ -0,0 +1,11 @@
+export type EnvironmentVariable = {
+    name: string,
+    value: string,
+    description: string
+}
+
+export type Secret = {
+    key: string,
+    value: string,
+    description: string
+}
---
+++ client/types/pipeline.ts
@@ -0,0 +1,98 @@
+import { EnvironmentVariable, Secret } from 'client/types/environment';
+
+export type PipelineArtifact = {
+    label: string
+    description?: string
+    source: string
+    destination: string
+}
+
+export type PipelineTask = {
+    command: string
+    label: string
+    description?: string
+    workingDirectory?: string
+    order: number
+    runIfPreviousTaskPassed: boolean
+    runIfPreviousTaskFailed: boolean
+    freezeBeforeRun: boolean
+    freezeAfterRun: boolean
+    taskOnSelfFailure: PipelineTask
+    environment?: EnvironmentVariable[]
+}
+
+export type PipelineTaskGroup = {
+    label: string
+    description?: string
+    order: number
+    environment?: EnvironmentVariable[]
+    tasks?: PipelineTask[]
+}
+
+export type PipelineJob = {
+    label: string
+    description?: string
+    elasticAgentId?: string
+    order: number
+    timeout: string
+    runType: string
+
+    taskGroups: PipelineTaskGroup[]
+    artifacts: PipelineArtifact[]
+    environment?: EnvironmentVariable[]
+}
+
+export type PipelineStage = {
+    label: string
+    description?: string
+    order: number
+    onlyOnPreviousSuccess?: boolean
+    fetchSources?: boolean
+    cleanupArtifacts?: boolean
+    cleanDirectory?: boolean
+    autoTriggerOnPreviousStageCompletion?: boolean
+    environment?: EnvironmentVariable[]
+    jobs?: PipelineJob[]
+}
+
+export type PipelineSource = {
+    label: string
+    description: string
+    targetPath: string
+    type: "git" | "svn" | "tfs"
+
+    branch: string
+    username: string
+    password: Secret
+    cloneDepth: number
+    pollingBehavior: string
+
+    denyList: string
+    invertFilter: boolean
+
+    url: string
+    pipelines: Pipeline[]
+}
+
+
+export type Pipeline = {
+    label: string
+    description: string
+    labelTemplate: string
+    state: "locked" | "paused" | "active"
+    lockingBehavior: "singleton" | "singletonNoFail" | "multiple"
+    group: string
+
+    automaticScheduling: boolean
+
+    cronSchedule: string
+    cronRunOnlyOnNewSource: boolean
+
+    trackingToolPattern: string
+    trackingToolUri: string
+
+    order: number
+    stages: PipelineStage[]
+    environment?: EnvironmentVariable[]
+    sources: PipelineSource[]
+}
