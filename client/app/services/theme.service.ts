338dc32e41be005cb590efb53b8feec8a8f3f600
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-07-13 22:28:50 -0400

    add light and dark icon directive

--- client/app/services/theme.service.ts
+++ client/app/services/theme.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Directive, ElementRef, Injectable, Input, ViewContainerRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { BehaviorSubject } from 'rxjs';
 
 type AppTheme = "light" | "dark";
@@ -6,8 +7,11 @@ type AppTheme = "light" | "dark";
 @Injectable({
     providedIn: 'root'
 })
-export class ThemeService extends BehaviorSubject<AppTheme>{
-    constructor() {
+export class ThemeService extends BehaviorSubject<AppTheme> {
+
+    constructor(
+        private sanitizer: DomSanitizer
+    ) {
         super("dark");
 
         this.subscribe(t => {
@@ -19,5 +23,42 @@ export class ThemeService extends BehaviorSubject<AppTheme>{
 
     public setTheme(t: AppTheme) {
         this.next(t);
+    }
+
+
+}
+
+
+@Directive({
+  selector: 'img[icon]',
+  standalone: true
+})
+export class ThemedIconDirective {
+
+    @Input('icon') icon: string;
+
+    mode: "dark" | "light" = "dark";
+    private _s;
+    constructor(
+        private element: ElementRef,
+        private theme: ThemeService
+    ) {
+    }
+
+    ngOnInit() {
+
+        this._s = this.theme.subscribe(t => {
+            this.mode = t;
+            this.updateSrc();
+        })
+    }
+
+    updateSrc() {
+        const img = this.element?.nativeElement as HTMLImageElement;
+        img.src = `/assets/icons/${this.icon}-${this.mode}.svg`;
+    }
+
+    ngOnDestroy() {
+        this._s.unsubscribe();
     }
 }
