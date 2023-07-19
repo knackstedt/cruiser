f2acbb92d98a6e4d33d688fefbc32e43d5c4cd25
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-07-18 22:53:37 -0400

    Add shared sorting method

--- client/app/services/utils.ts
+++ client/app/services/utils.ts
@@ -5,6 +5,11 @@
  */
 export const sleep = ms => new Promise(r => setTimeout(r, ms));
 
+export const orderSort = (a, b) => {
+    if (typeof a.order != 'number') return 1;
+    if (typeof b.order != 'number') return -1;
+    return a.order - b.order;
+};
 
 /**
  * Helper to update the page URL.
