26d20af36702fddbfbce3a8945233a5fae634c57
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-10-14 19:56:24 -0400

    ⚙chore(registry) use relative imports

--- client/app/component.registry.ts
+++ client/app/component.registry.ts
@@ -2,11 +2,11 @@ import { ComponentRegistration } from '@dotglitch/ngx-common';
 
 
 export const Pages: ComponentRegistration[] = [
-    { id: 'Login', load: () => import('client/app/pages/login/login.component'), hidden: true },
-    // { id: 'Dashboard', load: () => import('client/app/pages/dashboard/dashboard.component'), icon: "dashboard" },
+    { id: 'Login', load: () => import('./pages/login/login.component'), hidden: true },
+    // { id: 'Dashboard', load: () => import('./pages/dashboard/dashboard.component'), icon: "dashboard" },
 
-    { id: 'Pipelines', load: () => import('client/app/pages/pipelines/pipelines.component'), icon: "line_axis" },
-    { id: 'Agents', load: () => import('client/app/pages/agents/agents.component'), icon: "dns" },
+    { id: 'Pipelines', load: () => import('./pages/pipelines/pipelines.component'), icon: "line_axis" },
+    { id: 'Agents', load: () => import('./pages/agents/agents.component'), icon: "dns" },
     // { id: 'Configuration', load: () => import('client/app/pages/configuration/configuration.component'), icon: "settings" },
     // { id: 'Repos', load: () => import('client/app/pages/repos/repos.component'), icon: "https://git-scm.com/images/logos/downloads/Git-Icon-White.svg" },
     // { id: 'Analytics', load: () => import('client/app/pages/analytics/analytics.component'), icon: "analytics" },
