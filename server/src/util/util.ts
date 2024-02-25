78d88ec01f7bf191e74a1b43ed72f122bf76a666
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-02-25 11:33:45 -0500

    ⚙chore(util) add heap stats to init message

--- server/src/util/util.ts
+++ server/src/util/util.ts
@@ -124,6 +124,24 @@ export const getLogger = (file: string) => pino({
     }
 });
 
-export const logger = getLogger(`verbose-${process.pid}`);
+const _logger = getLogger(`verbose-${process.pid}`);
 
-logger.info({ message: "Application Bootup" });
+const heapStats = require('v8').getHeapStatistics();
+const toGb = (stat) => {
+    if (stat > 1024 ** 3)
+        return (stat / 1024 ** 3).toFixed(2) + "GB";
+    if (stat > 1024 ** 2)
+        return (stat / 1024 ** 2).toFixed(2) + "MB";
+    if (stat > 1024)
+        return (stat / 1024).toFixed(2) + "KB";
+    return stat + "B";
+};
+
+_logger.info({
+    message: "Application Started",
+    ...Object.entries(heapStats).map(([k, v]) => ({
+        [k]: toGb(v)
+    })).reduce((a, b) => ({ ...a, ...b }), {})
+});
+
+export const logger = _logger;
