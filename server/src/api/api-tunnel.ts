52be7a7511af94f90ec6f8b937c8b50b7a4e6a4b
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-03-06 08:06:44 -0500

    🚀feat(api-tunnel) create outbound connection to pod

--- server/src/api/api-tunnel.ts
+++ server/src/api/api-tunnel.ts
@@ -9,30 +9,50 @@ import { JobInstance } from '../../types/agent-task';
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+const k8sBatchApi = kc.makeApiClient(k8s.BatchV1Api);
 
 const router = express.Router();
+
+const podCache: { [key: string]: k8s.V1Pod } = {};
 
 router.use('/:id', route(async (req, res, next) => {
 
     const id = req.params['id'];
 
-    const [{ result: job }] = await db.query<JobInstance[]>(`SELECT * FROM jobs:${id}`);
+    const [{ result: jobs }] = await db.query<JobInstance[][]>(`SELECT * FROM ${id}`);
+    const [ job ] = jobs;
 
     if (!job)
         return next(404);
 
-    const { body: pod } = await k8sApi.readNamespacedPod(job.kubePodName, job.kubeNamespace)
+    const uid = job['jobUid'];
+    const jobId = job.id.split(':')[1];
+
+    // Caching mechanism so that we can persist the IP addresses without constant
+    // lookups
+    if (!podCache[uid]) {
+        const { body: pods } = await k8sApi.listNamespacedPod(job.kubeNamespace);
+
+        podCache[uid] = pods.items.find(i =>
+            i.metadata?.annotations?.['Job_Id'] == jobId
+        );
+    }
+    const pod = podCache[uid];
 
     const ip = pod.status.podIP;
     if (!ip)
         return next(404);
 
-    const prox = proxy(`http://${ip}:8080`, {
+    const prox = proxy(`http://${ip.replace(/\./g, '-')}.${job.kubeNamespace}.pod.cluster.local:8080`, {
         reqAsBuffer: true,
         reqBodyEncoding: null,
         parseReqBody: false,
         preserveHostHdr: false,
-        limit: '50mb'
+        limit: '50mb',
+        proxyReqOptDecorator(proxyReqOpts, srcReq) {
+            proxyReqOpts.headers['Authorization'] = job['kubeAuthnToken'];
+            return proxyReqOpts;
+        },
     });
     prox(req, res, next);
 }));
