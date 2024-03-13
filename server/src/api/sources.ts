c01d29c24fd2aed58bd58607d962edac0821a91b
GoCD Automation <no-reply@dotglitch.dev>
2024-03-13 19:24:07 -0400

    🚀feat(sources) add git api support to resolve branches and tags

--- server/src/api/sources.ts
+++ server/src/api/sources.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import { route } from '../util/util';
 import { db } from '../util/db';
+import execa from "execa";
 
 const router = express.Router();
 
@@ -9,6 +10,47 @@ router.get('/', route(async (req, res, next) => {
         .then(([data]) => res.send(data))
         .catch(err => next(err));
 }));
+
+const getRefs = async(remote: string) => {
+    // TODO: investigate how costly this execution is.
+    const { stdout } = await execa("git", ["ls-remote", remote]);
+
+    const obj: { hash: string, id: string }[] = [];
+
+    stdout.split('\n').forEach(line => {
+        const [hash, id] = line.split(/\s+/);
+        obj.push({ hash, id });
+    });
+    return obj;
+}
+
+router.get('/branches', (req, res, next) => {
+    getRefs(req.query.remote ?? req.body.remote)
+        .then(refs =>
+            refs
+                .filter(r => r.id[5] == "h") // Cheapest CPU check
+                .map(r => r.id.slice(11))    // Cheapest way to crop string
+        )
+        .then(data => res.send(data))
+        .catch(err => next(err));
+});
+
+router.get('/tags', (req, res, next) => {
+    getRefs(req.query.remote ?? req.body.remote)
+        .then(refs =>
+            refs
+                .filter(r => r.id[5] == "t") // Cheapest CPU check
+                .map(r => r.id.slice(10))    // Cheapest way to crop string
+        )
+        .then(data => res.send(data))
+        .catch(err => next(err));
+});
+
+router.get('/remotes', (req, res, next) => {
+    getRefs(req.query.remote ?? req.body.remote)
+        .then(refs => res.send(refs))
+        .catch(err => next(err));
+});
 
 
 export const SourcesApi = router;
