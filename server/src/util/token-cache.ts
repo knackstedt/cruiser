c562c30b69f498d69f7806a3bd4263802a27d598
GoCD Automation <no-reply@dotglitch.dev>
2024-03-13 22:15:03 -0400

    ⚙chore(token-cache) store tokens

--- server/src/util/token-cache.ts
+++ server/src/util/token-cache.ts
@@ -1,4 +1,16 @@
+import { db } from './db';
+
 const tokenCache: string[] = [];
 
-export const GetJobToken = (token) => tokenCache.includes(token);
-export const SetJobToken = (token: string) => tokenCache.push(token);
+export const CheckJobToken = async (token: string) => {
+    if (tokenCache.includes(token))
+        return true;
+
+    const [results] = await db.query<any[]>("select * from agent_tokens where token = $p", { p: token });
+
+    return results.length == 1;
+};
+export const SetJobToken = (token: string) => {
+    tokenCache.push(token);
+    db.create(`agent_tokens:ulid()`, { token })
+};
