44297cdd02979abfdcfa9849d282cc42fb2b3d00
GoCD Automation <no-reply@dotglitch.dev>
2024-03-20 21:34:25 -0400

    ⚙chore(process-manager) wrap logic in try/catch

--- agent/src/util/process-manager.ts
+++ agent/src/util/process-manager.ts
@@ -28,56 +28,66 @@ export const RunProcess = async (
         if (!await exists(task.workingDirectory || environment.buildDir))
             await mkdir(task.workingDirectory || environment.buildDir, { recursive: true });
 
-        const process = await new Promise<ChildProcessWithoutNullStreams>((res, rej) => {
+        let process: ChildProcessWithoutNullStreams;
+        try {
+            process = await new Promise<ChildProcessWithoutNullStreams>((res, rej) => {
+                try {
+                    // TODO: join env separately.
+                    // const specifiedCommand = task.taskScriptArguments['command'];
+                    const {
+                        command,
+                        args,
+                        env
+                    } = ParseCommand(task.taskScriptArguments['command'] + ' ' + task.taskScriptArguments['arguments']);
 
-            // TODO: join env separately.
-            // const specifiedCommand = task.taskScriptArguments['command'];
-            const {
-                command,
-                args,
-                env
-            } = ParseCommand(task.taskScriptArguments['command'] + ' ' + task.taskScriptArguments['arguments']);
+                    logger.info({
+                        msg: `Spawning process`,
+                        command: command,
+                        args,
+                        task
+                    });
 
-            logger.info({
-                msg: `Spawning process`,
-                command: command,
-                args,
-                task
-            });
+                    const process = spawn(command, args, {
+                        env: env,
+                        cwd: task.workingDirectory || environment.buildDir,
+                        timeout: task.commandTimeout || 0,
+                        windowsHide: true
+                    });
 
-            const process = spawn(command, args, {
-                env: env,
-                cwd: task.workingDirectory || environment.buildDir,
-                timeout: task.commandTimeout || 0,
-                windowsHide: true
-            });
+                    process.stdout.on('data', (data) => logger.socket.emit("log:stdout", { time: Date.now(), data }));
+                    process.stderr.on('data', (data) => logger.socket.emit("log:stderr", { time: Date.now(), data }));
 
-            process.stdout.on('data', (data) => logger.socket.emit("log:stdout", { time: Date.now(), data }));
-            process.stderr.on('data', (data) => logger.socket.emit("log:stderr", { time: Date.now(), data }));
+                    process.on('error', (err) => logger.error(err));
 
-            process.on('error', (err) => logger.error(err));
+                    process.on('disconnect', (...args) => {
+                        logger.error({
+                            msg: `Process unexpectedly disconnected`,
+                            args
+                        });
+                        res(process);
+                    });
 
-            process.on('disconnect', (...args) => {
-                logger.error({
-                    msg: `Process unexpectedly disconnected`,
-                    args
-                });
-                res(process);
-            });
-
-            process.on('exit', (code) => {
-                if (code == 0) {
-                    logger.info({ msg: `Process exited successfully` });
-                    res(process);
+                    process.on('exit', (code) => {
+                        if (code == 0) {
+                            logger.info({ msg: `Process exited successfully` });
+                            res(process);
+                        }
+                        else {
+                            logger.error({ msg: `Process exited with non-zero exit code`, code });
+                            res(process);
+                        }
+                    });
                 }
-                else {
-                    logger.error({ msg: `Process exited with non-zero exit code`, code });
-                    res(process);
+                catch(err) {
+                    rej(err);
                 }
             });
-        });
+        }
+        catch(err) {
+            logger.error(err);
+        }
 
-        if (process.exitCode == 0) {
+        if (process?.exitCode == 0) {
             logger.info({
                 msg: `Completed task ${task.label}`,
                 process,
