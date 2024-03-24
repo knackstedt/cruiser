a9cdda755da9fb08acdb954bb3be68e239b3fa21
GoCD Automation <no-reply@dotglitch.dev>
2024-03-24 17:09:47 -0400

    🐛fix(agent) add trailing slash

--- agent/src/util/environment.ts
+++ agent/src/util/environment.ts
@@ -5,7 +5,7 @@ export default {
     surrealNamespace: process.env['DOTGLITCH_DOTOPS_CLUSTER_NAMESPACE']?.trim() || 'dotglitch',
     cruiserToken: process.env['CRUISER_SERVER_TOKEN'],
     freezePollInterval: parseInt(process.env['DOTOPS_FREEZE_POLL_INTERVAL'] || '5000'),
-    buildDir: process.cwd() + '/build',
+    buildDir: process.cwd() + '/build/',
     jobInstanceId: `job_instance:` + process.env['CRUISER_AGENT_ID'].toUpperCase(),
     agentIsWindowsHost: os.platform() == "win32"
 }
