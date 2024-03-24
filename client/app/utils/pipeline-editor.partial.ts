4395dcdfd4b7818785e7fdf707430a2dc3e162c4
GoCD Automation <no-reply@dotglitch.dev>
2024-03-24 13:27:34 -0400

    🐛fix(pipeline-editor) fix state issues

--- client/app/utils/pipeline-editor.partial.ts
+++ client/app/utils/pipeline-editor.partial.ts
@@ -15,6 +15,9 @@ export class PipelineEditorPartial {
     @Input('pipeline') _pipeline: PipelineDefinition;
     @Input('new') isNewPipeline: any;
 
+    /**
+     * Cloned instance of the pipeline.
+     */
     public pipeline: PipelineDefinition = {} as any;
     isUnsavedState = false;
     isRestoredSave = false;
@@ -27,8 +30,7 @@ export class PipelineEditorPartial {
 
     }
 
-    private initPipelineObject() {
-        const p = this.pipeline;
+    private initPipelineObject(p: PipelineDefinition) {
 
         p.sources = p.sources ?? [];
         p.stages = p.stages ?? [];
@@ -50,8 +52,7 @@ export class PipelineEditorPartial {
                             }
                         ],
                         label: "Job 1",
-                        order: 0,
-                        platform: "kube_foo"
+                        order: 0
                     }
                 ]
             });
@@ -61,10 +62,15 @@ export class PipelineEditorPartial {
     }
 
     async ngOnInit() {
-        if (this.pipeline_id && !this._pipeline) {
+        if (this.pipeline_id && (this._pipeline as any) != "new" && !this._pipeline) {
             this.fetch.get(`/api/odata/${this.pipeline_id}`)
                 .then(res => {
-                    this._pipeline = res as any;
+                    if (res == null) {
+                        this.pipeline_id = null;
+                    }
+                    else {
+                        this._pipeline = res as any;
+                    }
                     this.ngOnInit();
                 })
                 .catch(err => {
@@ -73,25 +79,18 @@ export class PipelineEditorPartial {
             return;
         }
 
-        if (!this._pipeline?.id) {
-            this._pipeline = {
-                _isUserEditInstance: true
-            } as any;
-        }
-
-        if (typeof this._pipeline.id == "string") {
+        if (typeof this._pipeline?.id == "string") {
             const url = `/api/odata/pipeline?$filter=_isUserEditInstance eq true and _sourceId eq '${this._pipeline.id}' and _userEditing eq '${this.user.value.login}'`;
             const previouslyEdited = await this.fetch.get<any>(url);
 
             if (previouslyEdited.value.length > 0) {
-                this.pipeline = previouslyEdited.value[0];
-                this.initPipelineObject();
+                this.initPipelineObject(previouslyEdited.value[0]);
 
                 this.isUnsavedState = true;
                 this.isRestoredSave = true;
             }
             else {
-                this.pipeline = await this.fetch.put(`/api/odata/pipeline:${ulid()}`, {
+                const p = await this.fetch.put<PipelineDefinition>(`/api/odata/pipeline:${ulid()}`, {
                     ...this._pipeline,
                     _sourceId: this._pipeline.id,
                     id: undefined,
@@ -99,21 +98,29 @@ export class PipelineEditorPartial {
                     _isUserEditInstance: true,
                     _userEditing: this.user.value.login
                 });
+                this.initPipelineObject(p);
             }
         }
         else {
-            this.pipeline = await this.fetch.post(`/api/odata/pipeline/`, {
+            const p = await this.fetch.post<PipelineDefinition>(`/api/odata/pipeline/`, {
                 label: 'My new Pipeline',
                 state: 'new',
+                kind: this.pipelineKind,
                 _isUserEditInstance: true,
                 _userEditing: this.user.value.login,
                 order: -1,
-                group: this._pipeline.group || 'default',
+                group: this._pipeline?.group || 'default',
                 stages: [],
                 sources: []
             });
-            this._pipeline.id = `pipeline:${ulid()}`;
-            this.initPipelineObject();
+            // this._pipeline.id = `pipeline:${ulid()}`;
+
+            // Update the URL to reflect the current scope.
+
+            this.initPipelineObject(p);
+
+            if (location.href.includes("/new"))
+                history.replaceState({}, null, location.href.replace('/new', '/' + this.pipeline.id));
         }
     }
 
@@ -134,7 +141,7 @@ export class PipelineEditorPartial {
             "@odata.editLink": undefined,
             "@odata.id": undefined
         };
-        const res = await this.fetch.put(`/api/odata/${this._pipeline.id}`, data) as any;
+        const res = await this.fetch.put(`/api/odata/${this._pipeline.id ?? ('pipeline:' + ulid())}`, data) as any;
         await this.fetch.delete(`/api/odata/${this.pipeline.id}`);
 
         location.href = "#/Pipelines";
