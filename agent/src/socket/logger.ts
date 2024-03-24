f6c6186015248ca56f46c038bb588806fe964354
GoCD Automation <no-reply@dotglitch.dev>
2024-03-24 13:16:29 -0400

    ⚙chore(loggger) write stdout in different format

--- agent/src/socket/logger.ts
+++ agent/src/socket/logger.ts
@@ -66,13 +66,23 @@ export const getSocketLogger = async(socket: Socket) => {
         },
         stdout: (obj: Object) => {
             const t = Date.now();
-            process.stdout.write(`log:stdout ${t};${decoder.decode(obj['data']).trim().replace(/[\r\n]/g, '\\n')}\n`);
-            socket.emit("log:stdout", { time: t, data: obj['data'] })
+            socket.emit("log:stdout", { time: t, data: obj['data'] });
+
+            const output = decoder.decode(obj['data']).trim();
+            output.split('\n')
+                .forEach(line =>
+                    process.stdout.write(`log:stdout ${t};${line}\n`)
+                )
         },
         stderr: (obj: Object) => {
             const t = Date.now();
-            process.stdout.write(`log:stderr ${t};${decoder.decode(obj['data']).trim().replace(/[\r\n]/g, '\\n')}\n`);
-            socket.emit("log:stderr", { time: t, data: obj['data'] })
+            socket.emit("log:stderr", { time: t, data: obj['data'] });
+
+            const output = decoder.decode(obj['data']).trim();
+            output.split('\n')
+                .forEach(line =>
+                    process.stdout.write(`log:stderr ${t};${line}\n`)
+                )
         }
     };
 }
