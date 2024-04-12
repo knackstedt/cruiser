49c7e4d55cea7a94c32a941a79a1d32ce103a393
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-04-11 20:39:09 -0400

    🛠refactor(server) move job flusher

--- server/src/singleton/job-flusher.ts
+++ server/src/singleton/job-flusher.ts
@@ -1,12 +1,9 @@
 import fs, { stat } from 'fs-extra';
 import * as k8s from '@kubernetes/client-node';
-import { logger } from './util/logger';
-import { db } from './util/db';
-import { JobInstance } from './types/agent-task';
-import { environment } from './util/environment';
-import { PipelineInstance } from './types/pipeline';
-import { RunStage } from './util/pipeline';
-import axios from 'axios';
+import { logger } from '../util/logger';
+import { db } from '../util/db';
+import { JobInstance } from '../types/agent-task';
+import { environment } from '../util/environment';
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
@@ -91,7 +88,6 @@ export const WatchAndFlushJobs = async() => {
     SweepJobs();
 };
 
-
 // In addition to the watchers, we will check every 30 seconds
 // for jobs that may have fallen through the cracks.
 // This helps us handle server restarts without leaving jobs up in the air
@@ -175,110 +171,5 @@ const SaveLogAndCleanup = async (pod: k8s.V1Pod, job: k8s.V1Job) => {
     // Cleanup the job now that the log has been persisted
     await k8sBatchApi.deleteNamespacedJob(job.metadata.name, job.metadata.namespace);
     await k8sApi.deleteNamespacedPod(pod.metadata.name, pod.metadata.namespace);
-
-    processJobTriggers(job, jobInstance);
 }
 
-const processJobTriggers = async (job: k8s.V1Job, jobInstance: JobInstance) => {
-    // TODO: Process triggers for the subsequent jobs
-
-    const isFailure = jobInstance.state == "failed";
-
-    const [pipelineInstance] = await db.select<PipelineInstance>(job.metadata.annotations['cruiser.dev/pipeline-instance-id']);
-
-    const stage = pipelineInstance.spec.stages.find(s => s.id == job.metadata.annotations['cruiser.dev/stage-id']);
-
-    const jobInstances = await db.query<JobInstance[]>(`select * from job_instance where pipeline_instance = '${pipelineInstance.id}'`);
-
-    // Project the job instances into the objects
-    jobInstances.forEach(ji => {
-        const stage = pipelineInstance.spec.stages.find(s => s.id == ji.stage);
-
-        if (!stage) return;
-        stage['_instances'] = stage['_instances'] ?? [];
-        stage['_instances'].push(ji);
-
-        const job = stage.jobs.find(j => j.id == jobInstance.job);
-        if (!job) return;
-
-        job['_instance'] = ji;
-    });
-
-    // Execute all of the webhooks.
-    for (const webhook of (stage.webhooks ?? [])) {
-        // If the job failed, only execute the webhooks that
-        // are configured tp run on failuire.
-        if (isFailure && !webhook.executeOnFailure)
-            continue;
-        if (webhook.disabled)
-            continue;
-
-        const headers = webhook.headers
-            ?.map(([k, v]) => ({ [k]: v }))
-            .reduce((a, b) => ({ ...a, ...b }), {});
-
-        await axios({
-            method: webhook.method,
-            url: webhook.url,
-            data: webhook.body,
-            headers: headers,
-            proxy: webhook.proxy
-        })
-        .then(res => {
-            webhook.state = 'success';
-        })
-        .catch(err => {
-            logger.warn({
-                msg: "Executing webhook event for stage failed",
-                stage: stage.id,
-                webhook,
-                err
-            });
-
-            webhook.state = 'fail';
-        })
-    }
-
-    const stagesToTrigger = pipelineInstance.spec.stages
-        .filter(s => s.stageTrigger?.includes(stage.id));
-
-    // TODO: Handle approval triggers
-    if (stagesToTrigger.length > 0) {
-        // Trigger all of the stages that need to be run.
-        for (const stage of stagesToTrigger) {
-            if (stage.disabled)
-                continue;
-
-            // If the stage has approvals, don't automatically trigger it.
-            if (stage.requiredApprovals > 0) {
-                // Mark the stage as ready for approval
-                pipelineInstance.status.stageApprovals = pipelineInstance.status.stageApprovals ?? [];
-                pipelineInstance.status.stageApprovals.push({
-                    stageId: stage.id,
-                    readyForApproval: true,
-                    approvalCount: 0,
-                    approvers: [],
-                    hasRun: false
-                })
-                await db.merge(pipelineInstance.id, pipelineInstance);
-                continue;
-            }
-
-            // If the job failed, only execute the webhooks that
-            // are configured tp run on failuire.
-            if (isFailure && !stage.executeOnFailure)
-                continue;
-
-            // Run the stage
-            await RunStage(pipelineInstance, stage);
-        }
-    }
-    else {
-        // If there are no pipelines left to run, we'll close out the
-        // pipeline instance and collect stats for the run
-        pipelineInstance.status.phase = "stopped";
-        pipelineInstance.status.endEpoch = Date.now();
-        pipelineInstance.stats.totalRuntime = pipelineInstance.status.endEpoch - pipelineInstance.status.startEpoch;
-        await db.merge(pipelineInstance.id, pipelineInstance);
-    }
-}
