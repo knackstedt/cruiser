270a8b674d0d0494ea37a8e7e8255b491f307606
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-09-26 19:49:24 -0400

    ⚙chore(client) disable progress distractor animation as it's tanking perf

--- client/src/app/pages/lockout/lockout.component.ts
+++ client/src/app/pages/lockout/lockout.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from 'src/app/services/user.service';
-import gsap from 'gsap';
 
 @Component({
     selector: 'app-lockout',
