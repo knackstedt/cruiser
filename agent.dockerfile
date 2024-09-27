00ece6c2f8f5954c459721f2b29d5bfec53b6b86
Andrew G. Knackstedt <knackstedt@outlook.com>
2024-09-27 09:29:32 -0400

    ⚡perf(agent) drastically decrease docker image size - This removes the filesystem API that the pod exposed. To be re-implemented over websockets (which drastically simplifies the architecture)

--- agent.dockerfile
+++ agent.dockerfile
@@ -1,8 +1,7 @@
-FROM node:20-alpine
+FROM node:20-alpine as build
 # FROM oven/bun:alpine
 
-# Add required base dependencies
-RUN apk add --no-cache lrzip lrzip-extra-scripts
+# Add required build dependencies
 RUN apk add --no-cache python3 make g++ git
 
 COPY ./agent /agent
@@ -15,13 +14,19 @@ COPY ./types /agent/src/types
 
 WORKDIR /agent
 
+# Install and build the agent codebase
+RUN npm i
+RUN npm run build:docker
+RUN npm i --omit=dev
+
+FROM node:20-alpine
+RUN apk add --no-cache lrzip lrzip-extra-scripts
+COPY --from=build /agent/dist /agent
+COPY --from=build /agent/node_modules /agent/node_modules
+
 RUN chown 1000 /agent -R
 
 USER 1000
-
-# Install general agent deps
-RUN npm i
-RUN npm run build:docker
 
 EXPOSE 8080
 
--- agent/src/main.ts
+++ agent/src/main.ts
@@ -1,11 +1,8 @@
 import './util/instrumentation';
-import express from 'express';
-import http from 'http';
 
-import { HTTPLogger, logger } from './util/logger';
+import { logger } from './util/logger';
 import { RunAgentProcess } from './agent';
 import { environment } from './util/environment';
-import { FilesystemApi } from './api/filesystem';
 import { OpenTelemetry } from './util/instrumentation';
 
 if (
@@ -15,52 +12,6 @@ if (
     logger.fatal({ message: "Invalid agent identifier!"})
     process.exit(1);
 }
-
-(async () => {
-    // TODO: Replace this implementation with a websocket filesystem
-    const app = express();
-
-    // Handle a ping endpoint to check if this is even up
-    app.use("/ping", (req, res, next) => res.send(Date.now().toString()));
-    app.use((req, res, next) => {
-        // Ensure the access is only coming from the system that spawned this agent
-        if (environment.cruiserToken == req.get("X-Cruiser-Token"))
-            return next();
-
-        res
-            .status(401)
-            .send();
-    })
-
-    app.use(express.json());
-    app.use(HTTPLogger);
-
-    app.use("/fs", FilesystemApi);
-
-    app.use((req, res, next) => next(404));
-    app.use((err, req, res, next) => {
-        logger.error(err);
-
-        res
-            .status(500)
-            .send(err.hasOwnProperty("isAxiosError") ? {
-                message: err.message,
-                status: err.status,
-                code: err.code,
-                headers: err.config.headers,
-                url: err.config.url,
-                data: err.response?.data,
-                responseHeaders: err.response?.headers
-            } : err);
-    });
-
-    const server = http.createServer(app);
-    const port = 8080;
-    server.listen(port);
-
-    server.on("error", err => logger.error(err));
-    server.on("listening", () => logger.info(`Server listening on port ${port}`));
-})();
 
 RunAgentProcess(environment.jobInstanceId)
     .then(async () => {
